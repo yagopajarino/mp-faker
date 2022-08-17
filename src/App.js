1d9178c596f728052546caa8eac13a3347c775cd
Yago Pajariño <yagopajarino@outlook.com>
2022-08-17 13:09:07 -0300

    Fix bug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export default function App() {
   const [monto, setMonto] = useState(0);
   const [local, setLocal] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/home");
+  }, []);
+
   return <Outlet context={[monto, setMonto, local, setLocal]} />;
 }
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
           <Route path="/post" element={<Faker />}></Route>
         </Route>
       </Routes>
